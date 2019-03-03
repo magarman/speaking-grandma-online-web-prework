ea0877e96e4091c7590d3e322fd931bd25616c11
Mariana Garman <garman.mariana@gmail.com>
2019-03-03 17:11:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase="I LOVE YOU GRANDMA!")
-  if phrase == "Hi!" && 3<1
-   return "HUH?! SPEAK UP, SONNY!"
+  if phrase == "Hi!"
+   return "NO, NOT SINCE 1938!"
    elsif phrase == "Hi Nana, how are you?"
    return "HUH?! SPEAK UP, SONNY!"
    elsif phrase == "I LOVE YOU GRANDMA!" && 6+3 == 9
    return "I LOVE YOU TOO PUMPKIN!"
    else 
-     return "NO, NOT SINCE 1938!"
+     return "HUH?! SPEAK UP, SONNY!"
   end
 end
