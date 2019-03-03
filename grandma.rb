dd1496169fa42399901b76cea87ef35940fe24a4
Mariana Garman <garman.mariana@gmail.com>
2019-03-03 16:59:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase="I love you, grandma")
   if phrase == phrase.upcase
-   return "NO, NOT SINCE 1938!"
-   elsif phrase == !phrase.downcase
+   return "HUH?! SPEAK UP, SONNY!"
+   elsif phrase == !phrase.upcase
    puts "I LOVE YOU TOO PUMPKIN!"
    else 
      puts "SPEAK LOUDER, SONNY"
